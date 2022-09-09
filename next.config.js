c5df0eadc13c42caf2c5272bb5646ca4c244a3de
LegendNihal <adinnihalnz@gmail.com>
2022-09-09 20:07:26 +0600

    initial

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+      CONTENTFUL_SPACE_ID: "glivygyinstz",
+      CONTENTFUL_ACCESS_KEY: "-vR4b5bddHWialu-0ccxQuFPbJkpCyd6NzLvW6UruE8",
+    },
+    images: {
+      domains: ['images.ctfassets.net'],
+    }
+};
